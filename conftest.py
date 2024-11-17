486309cdfcaeb313ecd20a9948d03938c08935e4
Ivan <iegudyrev@gmail.com>
2024-11-17 17:08:47 +0300

    added try finally

--- conftest.py
+++ conftest.py
@@ -39,116 +39,119 @@ def pytest_addoption(parser):
 
 @pytest.fixture()
 def browser(request):
-    browser_name = request.config.getoption("--browser")
-    url = request.config.getoption("--url")
-    headless = request.config.getoption("--headless")
-    yadriver = request.config.getoption("--yadriver")
-    log_level = request.config.getoption("--test_log_level")
-    
-    executor = request.config.getoption("--executor")
-    remote = request.config.getoption("--remote")
-    browser_version = request.config.getoption("--bv")
-    vnc = request.config.getoption("--vnc")
-    video = request.config.getoption("--video")
-    
-
-    browser_logger = logging.getLogger(request.node.name)
-    browser_logger.info(
-        "===> Test %s started at %s" % (request.node.name, datetime.datetime.now())
-    )
-
-    # если запуск удаленно
-    if remote:
-        executor_url = f"http://{executor}:4444/wd/hub"
-
-        if browser_name == "chrome":
-            options = ChromeOptions()
-        elif browser_name == "firefox":
-            options = FFOptions()
-        else:
-            raise ValueError('Only the "chrome" and "firefox" browsers are supported')
+    try:
+        browser_name = request.config.getoption("--browser")
+        url = request.config.getoption("--url")
+        headless = request.config.getoption("--headless")
+        yadriver = request.config.getoption("--yadriver")
+        log_level = request.config.getoption("--test_log_level")
         
-        capabilities = {
-                            "browserVersion": browser_version,
-                            "selenoid:options": {
-                                                    "enableVNC": vnc,
-                                                    "name": request.node.name,
-                                                    "screenResolution": "1920x1080",
-                                                    "enableVideo": video,
-                                                    "timeZone": "Europe/Moscow",
-                                                    "env": ["LANG=ru_RU.UTF-8", "LANGUAGE=ru:en", "LC_ALL=ru_RU.UTF-8"],
-                                                    "sessionTimeout": "30m"
-                                                },
-                            "acceptInsecureCerts": True,
-                        }
+        executor = request.config.getoption("--executor")
+        remote = request.config.getoption("--remote")
+        browser_version = request.config.getoption("--bv")
+        vnc = request.config.getoption("--vnc")
+        video = request.config.getoption("--video")
+        
 
-        for k, v in capabilities.items():
-            options.set_capability(k, v)
-
-        driver = webdriver.Remote(
-            command_executor=executor_url,
-            options=options,
+        browser_logger = logging.getLogger(request.node.name)
+        browser_logger.info(
+            "===> Test %s started at %s" % (request.node.name, datetime.datetime.now())
         )
 
-    # если запуск локально
-    else:
-        if browser_name == "chrome":
-            options = ChromeOptions()
-            if headless:
-                options.add_argument("headless=new")
-            driver = webdriver.Chrome(service=ChromiumService(), options=options)
+        # если запуск удаленно
+        if remote:
+            executor_url = f"http://{executor}:4444/wd/hub"
 
-        elif browser_name == "firefox":
-            options = FFOptions()
-            if headless:
-                options.add_argument("--headless")
-            driver = webdriver.Firefox(options=options, service=FFService())
+            if browser_name == "chrome":
+                options = ChromeOptions()
+            elif browser_name == "firefox":
+                options = FFOptions()
+            else:
+                raise ValueError('Only the "chrome" and "firefox" browsers are supported')
+            
+            capabilities = {
+                                "browserVersion": browser_version,
+                                "selenoid:options": {
+                                                        "enableVNC": vnc,
+                                                        "name": request.node.name,
+                                                        "screenResolution": "1920x1080",
+                                                        "enableVideo": video,
+                                                        "timeZone": "Europe/Moscow",
+                                                        "env": ["LANG=ru_RU.UTF-8", "LANGUAGE=ru:en", "LC_ALL=ru_RU.UTF-8"],
+                                                        "sessionTimeout": "30m"
+                                                    },
+                                "acceptInsecureCerts": True,
+                            }
 
-        elif browser_name == "yandex":
-            options = ChromeOptions()
-            if headless:
-                options.add_argument("headless=new")
-            options.binary_location = (
-                r"C:\Users\gie\AppData\Local\Yandex\YandexBrowser\Application\browser.exe"
+            for k, v in capabilities.items():
+                options.set_capability(k, v)
+
+            driver = webdriver.Remote(
+                command_executor=executor_url,
+                options=options,
             )
-            driver = webdriver.Chrome(
-                options=options, service=ChromiumService(executable_path=yadriver)
-            )
 
-        elif browser_name == "edge":
-            options = ChromeOptions()
-            if headless:
-                options.add_argument("headless=new")
-            driver = webdriver.Edge(service=ChromiumService(), options=options)
-
+        # если запуск локально
         else:
-            raise Exception("Driver not supported")
-    
-    
-    driver.log_level = log_level
-    driver.test_name = request.node.name
+            if browser_name == "chrome":
+                options = ChromeOptions()
+                if headless:
+                    options.add_argument("headless=new")
+                driver = webdriver.Chrome(service=ChromiumService(), options=options)
 
-    browser_logger.info("Browser %s started" % browser)
+            elif browser_name == "firefox":
+                options = FFOptions()
+                if headless:
+                    options.add_argument("--headless")
+                driver = webdriver.Firefox(options=options, service=FFService())
 
-    driver.maximize_window()
-    driver.get(url)
-    driver.url = url
+            elif browser_name == "yandex":
+                options = ChromeOptions()
+                if headless:
+                    options.add_argument("headless=new")
+                options.binary_location = (
+                    r"C:\Users\gie\AppData\Local\Yandex\YandexBrowser\Application\browser.exe"
+                )
+                driver = webdriver.Chrome(
+                    options=options, service=ChromiumService(executable_path=yadriver)
+                )
 
-    yield driver
+            elif browser_name == "edge":
+                options = ChromeOptions()
+                if headless:
+                    options.add_argument("headless=new")
+                driver = webdriver.Edge(service=ChromiumService(), options=options)
 
-    if request.node.status == "failed":
-        allure.attach(
-            name="failure_screenshot_from_hook",
-            body=driver.get_screenshot_as_png(),
-            attachment_type=allure.attachment_type.PNG,
+            else:
+                raise Exception("Driver not supported")
+        
+        
+        driver.log_level = log_level
+        driver.test_name = request.node.name
+
+        browser_logger.info("Browser %s started" % browser)
+
+        driver.maximize_window()
+        driver.get(url)
+        driver.url = url
+
+        yield driver
+
+        if request.node.status == "failed":
+            allure.attach(
+                name="failure_screenshot_from_hook",
+                body=driver.get_screenshot_as_png(),
+                attachment_type=allure.attachment_type.PNG,
+            )
+            allure.attach(
+                name="page_source",
+                body=driver.page_source,
+                attachment_type=allure.attachment_type.HTML,
+            )
+
+    finally:
+        driver.quit()
+        driver.close()
+        browser_logger.info(
+            "===> Test %s finished at %s" % (request.node.name, datetime.datetime.now())
         )
-        allure.attach(
-            name="page_source",
-            body=driver.page_source,
-            attachment_type=allure.attachment_type.HTML,
-        )
-
-    driver.quit()
-    browser_logger.info(
-        "===> Test %s finished at %s" % (request.node.name, datetime.datetime.now())
-    )
